b0ab77b3f9d5e9b9211e2dd5e798cfee729749ee
Paul Baker <paul.baker@wgu.edu>
2017-06-06 10:38:06 -0600

    Commiting demo files to version control
    Seeing is believing, let's share what works and demonstrate how maven
    runs.
    
    https://stackoverflow.com/a/44267571/1478636

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,29 @@
+import com.google.common.io.Files;
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by paul.baker on 5/30/17.
+ */
+public class DemoTest {
+
+    @Test
+    public void demonstrateScreenshotTest() throws IOException {
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("http://rads.stackoverflow.com/amzn/click/055338256X");
+        File screenshotFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        // This move is necessary, the original file is temporary and gets deleted after java exists
+        File resultingScreenshot = new File(System.getProperty("user.home"), "screenshot.png");
+        Files.move(screenshotFile, resultingScreenshot);
+        driver.quit();
+
+        System.out.println("The screenshot is found here: " + resultingScreenshot);
+    }
+}
